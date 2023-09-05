69aa1f74e11b864a1573c04ed4927dd637c21bf4
Mohamed Hassaan <mmhassaan3@gmail.com>
2023-09-05 11:07:28 +0400

    add to netlify

--- src/components/header/nav/Nav.jsx
+++ src/components/header/nav/Nav.jsx
@@ -5,7 +5,7 @@ import "../nav/nav.css"
 const Nav = () => {
   return (
     <div className="navbar">
-      <NavLink to="/ny-times-api">Home</NavLink>
+      <NavLink to="/">Home</NavLink>
       <NavLink to="/fashion">Fashion</NavLink>
       <NavLink to="/art">Art's</NavLink>
       <NavLink to="/us">U.S.</NavLink>
--- src/index.js
+++ src/index.js
@@ -2,15 +2,15 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./app/App";
 import { BrowserRouter } from "react-router-dom";
-import store  from "./app/store";
+import store from "./app/store";
 import { Provider } from "react-redux";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-      <Provider store={store}>
-        <App />
-      </Provider>
+        <Provider store={store}>
+          <App />
+        </Provider>
     </BrowserRouter>
   </React.StrictMode>
 );
